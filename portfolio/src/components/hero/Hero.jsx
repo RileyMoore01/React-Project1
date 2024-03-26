bff23158b8c8d7d6cce8a1e24d054b5d3ae15706
RileyMoore01 <xrileyxmoorex@outlook.com>
2024-03-25 22:25:58 -0500

    started on homepage

---
+++ portfolio/src/components/hero/Hero.jsx
@@ -0,0 +1,13 @@
+import './hero.scss'
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="image-container">
+                <img src="/portfolio/public/hero.png" alt=""></img>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
