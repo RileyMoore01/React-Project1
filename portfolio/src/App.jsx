d2593d447e07fc35231768bb7337bb7828a70c6e
Riley Moore <xrileyxmoorex@outlook.com>
2024-04-03 21:04:09 -0500

    contact page working

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,11 @@
 import"./app.scss"
+import Contact from "./components/contact/Contact";
+// import Cursor from "./components/cursor/Cursor";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Parallax from "./components/parallax/Parallax"
+import Parallax from "./components/parallax/Parallax";
+// import Portfolio from "./components/portfolio/Portfolio";
+// import Services from "./components/services/Services";
 // import Test from "./Test";
 
 const App = () => {
@@ -11,18 +15,17 @@ const App = () => {
       <Hero/>
     </section>
     <section id="Services"><Parallax type="services"/></section>
-    <section>Services</section>
+    {/* <section><Services/></section> */}
     <section id="Portfolio"><Parallax type="portfolio"/></section>
     <section>Portfolio1</section>
     <section>Portfolio2</section>
     <section>Portfolio3</section>
-    <section id="Contact Me"></section>
+    <section id="Contact"><Contact/></section>
     {/* <Test/> */}
   </div>;
 };
 
 export default App;
-
 
 // reference code:
 // https://www.youtube.com/watch?v=CHGHuF24Cjw&list=LL&index=3&t=263s
