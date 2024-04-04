134eb4ae520b9bc684ab7a1ba39f750ddf5e1a34
Riley Moore <xrileyxmoorex@outlook.com>
2024-04-03 21:08:58 -0500

    services page complete

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,7 +4,7 @@ import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
-// import Portfolio from "./components/portfolio/Portfolio";
+import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 // import Test from "./Test";
 
@@ -17,9 +17,7 @@ const App = () => {
     <section id="Services"><Parallax type="services"/></section>
     <section><Services/></section>
     <section id="Portfolio"><Parallax type="portfolio"/></section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
+    <Portfolio />
     <section id="Contact"><Contact/></section>
     {/* <Test/> */}
   </div>;
