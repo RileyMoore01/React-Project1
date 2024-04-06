337981b8554f67ece4f3312d5fcd86d02aa1cc97
Riley Moore <xrileyxmoorex@outlook.com>
2024-04-05 21:13:42 -0500

    touch ups on the cursor components

--- portfolio/src/components/cursor/Cursor.jsx
+++ portfolio/src/components/cursor/Cursor.jsx
@@ -1,25 +1,28 @@
-import "./cursor.scss"
-import { motion } from "framer-motion"
 import { useEffect, useState } from "react";
+import { motion } from "framer-motion";
+import "./cursor.scss";
 
 const Cursor = () => {
-    const [position, setPosition] = useState({ X: 0, y: 0});
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
-    useEffect(() => {
-        const mouseMove = (e) => {
-            setPosition({ x: e.clientX, y: e.clientY });
-        };
-        window.addEventListener("mousemove", mouseMove);
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
 
-        return () => {
-        window.removeEventListener("mousemove", mouseMove);
-        };
-    }, [] );
+    window.addEventListener("mousemove", mouseMove);
 
-    return (
-        <motion.div
-            className="cursor"
-            animate={{ x: position.x+10, y:position.y+10}}>
-        </motion.div>
-    );
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  }, []);
+
+  return (
+    <motion.div
+      className="cursor"
+      animate={{ x: position.x+10, y: position.y+10 }}
+    ></motion.div>
+  );
 };
+
+export default Cursor;
