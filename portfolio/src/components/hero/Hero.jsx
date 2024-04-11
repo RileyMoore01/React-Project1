bf9f5a29d0cb444a1d77abb74baf5353e9732ce9
Riley Moore <xrileyxmoorex@outlook.com>
2024-04-10 21:06:02 -0500

    small edit

--- portfolio/src/components/hero/Hero.jsx
+++ portfolio/src/components/hero/Hero.jsx
@@ -1,6 +1,6 @@
 import "./hero.scss";
 import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
 const textVariants = {
   initial: {
